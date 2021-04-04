7eb06e3abcca230c477ae0d39fb33ade5fb5ea97
Evgenia Vaartis <97nomad@gmail.com>
2021-04-04 22:48:54 +0300

    version 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TgScrumPoker.MixProject do
   def project do
     [
       app: :tg_scrum_poker,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
